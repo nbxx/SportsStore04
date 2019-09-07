06a737cdc722ff3b75803698948d88cc63b4bd90
yuguan <yuguan>
2019-09-07 16:41:14 -0400

    1 [CHG] add product repository;

--- Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -1,10 +1,13 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Entities;
 
 namespace Vic.SportsStore.WebApp
 {
@@ -17,6 +20,19 @@ namespace Vic.SportsStore.WebApp
             builder
                 .RegisterControllers(typeof(MvcApplication).Assembly)
                 .PropertiesAutowired();
+
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+
+            mock
+                .Setup(m => m.Products)
+                .Returns(new List<Product>
+                {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Surf board", Price = 179 },
+                    new Product { Name = "Running shoes", Price = 95 }
+                });
+
+            builder.RegisterInstance<IProductsRepository>(mock.Object);
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
