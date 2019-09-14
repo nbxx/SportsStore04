bd4c9ab9a54aa7e6fdadd6685d808d72cd1454d8
yuguan <yuguan>
2019-09-14 16:36:55 -0400

    1 [CHG] add category navi;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -14,12 +14,13 @@ namespace Vic.SportsStore.WebApp.Controllers
 
         public IProductsRepository ProductsRepository { get; set; }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
                 Products = ProductsRepository
                 .Products
+                .Where(p => category == null || p.Category == category)
                 .OrderBy(p => p.ProductId)
                 .Skip((page - 1) * PageSize)
                 .Take(PageSize),
@@ -28,7 +29,8 @@ namespace Vic.SportsStore.WebApp.Controllers
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = ProductsRepository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             };
 
             return View(model);
