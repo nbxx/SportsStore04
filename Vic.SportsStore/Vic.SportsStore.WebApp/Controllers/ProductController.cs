f86d9384b00247094da42322a34f06c0ef2b9bb0
yuguan <yuguan>
2019-09-14 15:39:02 -0400

    1 [CHG] add page info;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
@@ -15,12 +16,23 @@ namespace Vic.SportsStore.WebApp.Controllers
 
         public ViewResult List(int page = 1)
         {
-            return View(
-                ProductsRepository
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
                 .Products
                 .OrderBy(p => p.ProductId)
                 .Skip((page - 1) * PageSize)
-                .Take(PageSize));
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
+
     }
 }
