5bc296bf983c966d097059aa0b17399adb98c4df
yuguan <yuguan>
2019-09-28 17:22:16 -0400

    1 [CHG] register order processor;

--- Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/App_Start/IocConfig.cs
@@ -30,6 +30,11 @@ namespace Vic.SportsStore.WebApp
                 .RegisterInstance<EFDbContext>(new EFDbContext())
                 .PropertiesAutowired();
 
+            builder
+                .RegisterInstance<IOrderProcessor>(
+                new EmailOrderProcessor(new EmailSettings()))
+                .PropertiesAutowired();
+
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
