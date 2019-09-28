25986f03422678be6afeaab2cbe34f22a6f8ac03
yuguan <yuguan>
2019-09-28 17:51:08 -0400

    1 [BUG] fix bug, email write to file;

--- Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EmailOrderProcessor.cs
+++ Vic.SportsStore/Vic.SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -34,6 +35,11 @@ namespace Vic.SportsStore.Domain.Concrete
                     smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
                     smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
                     smtpClient.EnableSsl = false;
+
+                    if (!Directory.Exists(emailSettings.FileLocation))
+                    {
+                        Directory.CreateDirectory(emailSettings.FileLocation);
+                    }
                 }
 
                 StringBuilder body = new StringBuilder()
--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/CartController.cs
@@ -12,10 +12,14 @@ namespace Vic.SportsStore.WebApp.Controllers
     public class CartController : Controller
     {
         private IProductsRepository repository;
+        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductsRepository repo)
+        public CartController(
+            IProductsRepository repo,
+            IOrderProcessor proc)
         {
             repository = repo;
+            orderProcessor = proc;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -63,5 +67,26 @@ namespace Vic.SportsStore.WebApp.Controllers
         {
             return View(new ShippingDetails());
         }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
+        }
+
     }
 }
